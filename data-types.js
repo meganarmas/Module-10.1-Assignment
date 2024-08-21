286b05828540c29926b3b085a3f0ce249a8e4947
meganarmas <meganarmas@gmail.com>
2024-08-21 16:55:49 -0500

    first commit

---
+++ data-types.js
@@ -0,0 +1,15 @@
+let studentName;
+let studenAge;
+let studentGrade;
+let isPresent;
+
+
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10
+isPresent = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentage);
+console.log("Student Grade:", studentGrade)
+console.log("Status:", isPresent);
